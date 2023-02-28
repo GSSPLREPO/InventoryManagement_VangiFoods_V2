51ec717996d36859d8655b8e531bfca64559443c
GSSPLREPO <62415355+GSSPLREPO@users.noreply.github.com>
2023-02-28 17:51:18 +0530

    Commited By Snehal 28-02-2023
    Bug Fixes

--- InVanWebApp_BO/DailyMonitoringBO.cs
+++ InVanWebApp_BO/DailyMonitoringBO.cs
@@ -14,24 +14,40 @@ namespace InVanWebApp_BO
         [Required(ErrorMessage = "Select Date!")]
         [DataType(DataType.Date)]
         public Nullable<System.DateTime> Date { get; set; }
+        [Required(ErrorMessage = "Enter Personal Hygine!")]
         public string PersonalHygine { get; set; }
+        [Required(ErrorMessage = "Enter Cleaning & Sanitation!")]
         public string CleaningAndSanitation { get; set; }
+        [Required(ErrorMessage = "Enter Cleaning of Equipment!")]
         public string CleaningOfEquipment { get; set; }
+        [Required(ErrorMessage = "Enter Water Potability!")]
         public string WaterPotability { get; set; }
+        [Required(ErrorMessage = "Enter Allergic!")]
         public string Allergic { get; set; }
+        [Required(ErrorMessage = "Enter Non Allergic!")]
         public string NonAllergic { get; set; }
+        [Required(ErrorMessage = "Enter Vegetable Processing Area!")]
         public string VegetableProcessingArea { get; set; }
+        [Required(ErrorMessage = "Enter Packaging & Labelling Area!")]
         public string PackagingLabellingArea { get; set; }
+        [Required(ErrorMessage = "Enter FGS Area!")]
         public string FgsArea { get; set; }
+        [Required(ErrorMessage = "Enter Inside!")]
         public string Inside { get; set; }
+        [Required(ErrorMessage = "Enter Out Side!")]
         public string OutSide { get; set; }
+        [Required(ErrorMessage = "Enter Dry!")]
         public string Dry { get; set; }
+        [Required(ErrorMessage = "Enter Wet!")]
         public string Wet { get; set; }
+        [Required(ErrorMessage = "Enter Out Siders!")]
         public string OutSiders { get; set; }
+        [Required(ErrorMessage = "Enter Production Area!")]
         public string ProductionArea { get; set; }
+        [Required(ErrorMessage = "Enter Office Staff!")]
         public string OfficeStaff { get; set; }
 
-        [Required(ErrorMessage = "Enter name of User!")]
+        [Required(ErrorMessage = "Enter Name of User!")]
         public string VerifyByName { get; set; }
         public string Remark { get; set; }
         public Nullable<bool> IsDeleted { get; set; }
--- InVanWebApp_BO/PreStartupHygieneBO.cs
+++ InVanWebApp_BO/PreStartupHygieneBO.cs
@@ -10,27 +10,44 @@ namespace InVanWebApp_BO
     public class PreStartupHygieneBO
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Enter name of User!")]
+        [Required(ErrorMessage = "Enter Name of User!")]
         public string VerifyBy { get; set; }
         [Required(ErrorMessage = "Select Date!")]
         [DataType(DataType.Date)]
         public Nullable<System.DateTime> Date { get; set; }
+        [Required(ErrorMessage = "Enter RM Receving Area!")]
         public string RMRecevingArea { get; set; }
+        [Required(ErrorMessage = "Enter Crates Blue!")]
         public string CratesBlue { get; set; }
+        [Required(ErrorMessage = "Enter Crates Yellow!")]
         public string CratesYellow { get; set; }
+        [Required(ErrorMessage = "Enter Crates Red!")]
         public string CratesRed { get; set; }
+        [Required(ErrorMessage = "Enter Weighting Area!")]
         public string WeightingArea { get; set; }
+        [Required(ErrorMessage = "Enter Water!")]
         public string Water { get; set; }
+        [Required(ErrorMessage = "Enter Hygine Area!")]
         public string HygineArea { get; set; }
+        [Required(ErrorMessage = "Enter Raw Material!")]
         public string RawMaterial { get; set; }
+        [Required(ErrorMessage = "Enter Finish Goods!")]
         public string FinishGoods { get; set; }
+        [Required(ErrorMessage = "Enter Walk Way!")]
         public string WalkWay { get; set; }
+        [Required(ErrorMessage = "Enter Vegetable Washing Area!")]
         public string VegetableWashingArea { get; set; }
+        [Required(ErrorMessage = "Enter Peeling Machine!")]
         public string PeelingMachine { get; set; }
+        [Required(ErrorMessage = "Enter Cold Storage!")]
         public string ColdStorage { get; set; }
+        [Required(ErrorMessage = "Enter Roboqubos!")]
         public string Roboqubos { get; set; }
+        [Required(ErrorMessage = "Enter Silo!")]
         public string Silo { get; set; }
+        [Required(ErrorMessage = "Enter Packaging Line!")]
         public string PackagingLine { get; set; }
+        [Required(ErrorMessage = "Enter Chiller!")]
         public string Chiller { get; set; }
         public string Remark { get; set; }
         public Nullable<bool> IsDeleted { get; set; }
